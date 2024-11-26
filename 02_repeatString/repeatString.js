7bd92600f4c07dcdfaaecf79448860fcc6974985
ogcpy <bashdavidson@icloud.com>
2024-11-26 20:37:00 +0000

    repeating exercise with loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR"; // Return "ERROR" if the number is negative
+    }
 
+    let result = ""; // Initialize an empty string to store the result
+    for (let i = 0; i < times; i++) {
+        result += word; // Append the string to the result in each iteration
+    }
+    return result;
 };
 
 // Do not edit below this line
