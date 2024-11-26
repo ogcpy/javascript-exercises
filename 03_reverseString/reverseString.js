9766f41c3fd2526f160398527181bdf399b2b520
ogcpy <bashdavidson@icloud.com>
2024-11-26 20:41:28 +0000

    learning to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = ""; // Initialize an empty string to hold the reversed result
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i]; // Add each character from the end to the beginning
+    }
+    return reversed;
 };
 
 // Do not edit below this line
